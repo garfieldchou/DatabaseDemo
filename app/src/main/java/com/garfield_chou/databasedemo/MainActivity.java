abcdf7a355b28f9fb0a9a1accb54f869088b3482
garfield_chou <garfield_chou@asus.com>
2017-01-24 17:56:02 +0800

    Create a SQLite database and insert some data

--- app/src/main/java/com/garfield_chou/databasedemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/databasedemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.garfield_chou.databasedemo;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('garfield', 34)");
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
